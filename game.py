964f6d7760c149bceea625901f95935b12d9df44
Amor-Jansen <Amor.jansen2@gmail.com>
2023-01-18 14:54:36 +0000

    Update first question

--- game.py
+++ game.py
@@ -1,22 +1,9 @@
-def play_game():
+def game_data():
     """
-    All of the questions of the game.
-    if the user gets an answer right score will increase by 1.
+    This contains all the questions and answers
     """
-
-    score = 0
-
-    while True:
-        print("Who are the main two characters?")
-        answer1 = input(
-            "A.Sam and Dean \nB.Mike and Ike \nC.Joe and Bo \nAnswer: \n"
-        ).strip()
-        if answer1.upper() == "A":
-            print("Correct!\n")
-            score += 1
-            break
-        elif answer1.upper() == "B" or answer1.upper() == "C":
-            print("Incorrect!\n")
-            break
-        else:
-            print("Invalid input!! Please choose between 'A' 'B' or 'C'\n")
+    {"question": "Who are the main two characters?",
+     "answers": {"A": "Mike and Ike",
+                 "B": "Sam and Dean",
+                 "C": "Lou and Moe"},
+     "correct_answer": "B"},
