da8194ff9b40141f3d07b0017a271e71f4d4e604
Amor-Jansen <Amor.jansen2@gmail.com>
2023-01-18 13:25:32 +0000

    Add instructions to the user on naming conventions and add datetime

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,3 +13,24 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('supernatural-quiz')
 
+#Welcome message
+print("welcome to the 'Supernatural' quiz.\n")
+print("How well do you know the show?")
+
+def player_name():
+    """
+    This will get the player name.
+    the request will only end when a valid name is chosen.
+    """
+    while True:
+        print("Choose a user name.\n")
+        print("Only characters a-z, A-Z and 0-9 are permitted.")
+        print("Maximun of 9 characters.")
+        print("White space will be removed.\n")
+
+        datetime = datetime.now()
+        datetime_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+        name = input("Please enter your name: \n")
+
+        if 
