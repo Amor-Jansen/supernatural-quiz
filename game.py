080c56dfd82ad78d1ca5b15ee481a49f6a8cab86
Amor-Jansen <Amor.jansen2@gmail.com>
2023-01-19 10:18:52 +0000

    Add a count for the number of questions

--- game.py
+++ game.py
@@ -101,3 +101,17 @@ game_data = [
      "correct_answer": "B"},
     ]
     
+
+def count_keys(game_data):
+    """
+    This will counnt the number of questions and allow
+    more to be added or even taken away.
+    """
+    count = 0
+    for i in enumerate(game_data):
+        count += 1
+    return count
+
+
+num_of_questions = (count_keys(game_data))
+    
